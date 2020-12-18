b5ea5755bd9e896a9802a100505b9d39cd901dd3
Marqq <36154480+Marcgs96@users.noreply.github.com>
2020-12-18 10:53:11 +0100

    started data structure

---
+++ Assets/Script/EventData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventData
+{
+    public uint eventID;
+}
+
+public class KillEvent : EventData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public string enemyType;
+    public uint playerId;
+    public uint sessionId;
+    public float timeStamp;
+}
+
+public class DeathEvent : EventData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public string enemyType;
+    public uint playerId;
+    public uint sessionId;
+    public float timeStamp;
+}
+
+public class PositionEvent : EventData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public uint playerId;
+    public uint sessionId;
+    public float timeStamp;
+}
+
+public class LifeLostEvent : EventData
+{
+    public Vector3 position;
+    public uint playerId;
+    public uint sessionId;
+    public float timeStamp;
+}
+
+public class BoxDestroyedEvent : EventData
+{
+    public Vector3 position;
+    public uint playerId;
+    public uint sessionId;
+    public float timeStamp;
+}
+
+public class JumpEvent : EventData
+{
+    public Vector3 position;
+    public uint playerId;
+    public uint sessionId;
+    public float timeStamp;
+}
+
+
---
+++ Assets/Script/EventHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit3D;
+
+public class EventHandler : MonoBehaviour
+{
+    List<KillEvent> killEvents;
+    List<DeathEvent> deathEvents;
+    List<PositionEvent> positionEvents;
+    List<LifeLostEvent> lifeLostEvents;
+    List<BoxDestroyedEvent> boxDestroyedEvents;
+    List<JumpEvent> jumpEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FillKillEventData(Damageable damageable)
+    {
+        KillEvent newKillEvent = new KillEvent();
+        newKillEvent.position = damageable.gameObject.GetComponent<Transform>().position;
+        newKillEvent.rotation = damageable.gameObject.GetComponent<Transform>().rotation;
+
+    }
+
+    public void FillDeathEventData(Damageable damageable)
+    {
+        DeathEvent newKillEvent = new DeathEvent();
+        newKillEvent.position = damageable.gameObject.GetComponent<Transform>().position;
+    }
+
+    public void FillPositionEventData(Damageable damageable)
+    {
+        KillEvent newKillEvent = new KillEvent();
+        newKillEvent.position = damageable.gameObject.GetComponent<Transform>().position;
+    }
+
+    public void FillLifeLostEventData(Damageable damageable)
+    {
+        KillEvent newKillEvent = new KillEvent();
+        newKillEvent.position = damageable.gameObject.GetComponent<Transform>().position;
+    }
+
+    public void FillBoxDestroyedEventData(Damageable damageable)
+    {
+        KillEvent newKillEvent = new KillEvent();
+        newKillEvent.position = damageable.gameObject.GetComponent<Transform>().position;
+    }    
+    
+    public void FillJumpEventData(Damageable damageable)
+    {
+        KillEvent newKillEvent = new KillEvent();
+        newKillEvent.position = damageable.gameObject.GetComponent<Transform>().position;
+    }
+}
